5383da24d6024f9d6ff2b8ddc7cdfe558b0a3fb6
ksnldn <92630098+ksnldn@users.noreply.github.com>
2023-12-14 10:43:27 -0500

    setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="time_tracker",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "datetime",
+        "json",
+        "dateutil",
+        "decouple",
+        "pandas",
+        "base64",
+    ],
+)
