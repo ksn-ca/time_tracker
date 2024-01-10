497440d4bb28fa448598dc3f0f1cf1eaa503760e
ksnldn <92630098+ksnldn@users.noreply.github.com>
2024-01-10 16:57:08 -0500

    add clear console function and fix toggl bug

--- project/main.py
+++ project/main.py
@@ -23,15 +23,15 @@ FILE_EXISTS = os.path.isfile(ENV_FILE)
 
 # post_to_pixella_yesterday(YESTERDAY)
 # post_to_pixella_since(SINCE)
-
+def clear(): os.system('cls' if os.name=='nt' else 'clear')
 
 def edit_env_file(key, new_val):
     pass
 
 
 def create_env_file(text):
-    success = False 
-    try: 
+    success = False
+    try:
         with open(ENV_FILE, "w") as file:
             file.write(text)
         success = True
@@ -41,7 +41,7 @@ def create_env_file(text):
 
 
 def get_toogl_info():
-    success = False
+    clear()
     toggl_workspace_id = None
     toggl_token = input(
         "Please provide details of your Toggl account.\nToggl API token:   "
@@ -49,95 +49,107 @@ def get_toogl_info():
     toggl_response = check_toggl(toggl_token)
     if toggl_response["success"]:
         toggl_workspace_id = toggl_response["workspace_id"]
-        success = True
     else:
         while not toggl_response["success"]:
-            toggl_token = input(f"{toggl_response['message']}\nPlease try again or enter 'stop'.\nToggl API token:   ")
+            clear()
+            toggl_token = input(
+                f"{toggl_response['message']}\nPlease try again or enter 'stop'.\nToggl API token:   ").strip()
             if toggl_token.lower() == 'stop':
                 break
             toggl_response = check_toggl(toggl_token)
-
-    return toggl_token, toggl_workspace_id, success
+    return toggl_token, toggl_workspace_id, toggl_response['success']
 
 
 def get_pixella_info():
+    clear()
     success = False
     pixella_token = None
     pixella_username = None
-    user_input = input(
-        """Please choose the option:
-    1. Enter Pixella details manually (if you already have an account).
-    2. Create Pixella account now.
-    Enter either 1 or 2:   """
-    )
+    user_input = ''
+    while user_input not in ['1', '2']:
+        user_input = input(
+            """Please choose the option:
+        1. Enter Pixella details manually (if you already have an account).
+        2. Create Pixella account now.
+        Enter either 1 or 2:   """
+        ).strip()
 
-    if user_input == '1':
-        pixella_token = input('Please provide details of your Pixella account.\nPixella API token:   ')
-        pixella_username = input('Pixella username:   ')
-        
-        success = True
-        # CHECK if accurate
-    elif user_input == '2':
-        pass
+        if user_input == '1':
+            pixella_token = input(
+                'Please provide details of your Pixella account.\nPixella API token:   ')
+            pixella_username = input('Pixella username:   ')
+
+            success = True
+            # CHECK if accurate
+        elif user_input == '2':
+            pass
+        else:
+            print('Sorry, this input is not supported.')
+
 
     return pixella_token, pixella_username, success
 
 
+def create_env_text(toggl_token, toggl_workspace_id, pixella_token, pixella_username):
+    text = f"{API_NAMES['pixella']['token']}:{pixella_token}\n"
+    text += f"{API_NAMES['pixella']['name']}:{pixella_username}\n"
+    text += f"{API_NAMES['toggl']['token']}:{toggl_token}\n"
+    text += f"{API_NAMES['toggl']['workspace']}:{toggl_workspace_id}"
+    return text
+
+
+def restart_prompt(prompt=''):
+    clear()
+    print(prompt)
+    user_input = ''
+    while user_input not in ['1', '2']:
+        user_input = input(
+            """Would you like to:
+            1. Restart the app
+            2. Close the app
+            Enter 1 or 2:   """).strip()
+
+        if user_input == '1':
+            clear()
+            start_app()
+        elif user_input == '2':
+            pass
+        else:
+            clear()
+            print('Sorry, this input is not supported.')
 
 
 def start_app():
-    options = [""]
-    available_options = []
-    greeting_text = "Welcome to Toggl to Pixella app!\n"
-    print(greeting_text)
-
     if not FILE_EXISTS:
         user_input = input(
             """There's no information about your Toggl or Pixella accounts.
     Would you like to add it? (y/n)   """
-        )
+        ).strip().lower()
         if user_input in YES:
             toggl_token, toggl_workspace_id, toggl_success = get_toogl_info()
+            print('Toggl sucess in start app', toggl_success)
 
             if toggl_success:
                 pixella_token, pixella_username, pixella_success = get_pixella_info()
                 if pixella_success:
-                    text = f"{API_NAMES['pixella']['token']}:{pixella_token}\n"
-                    text += f"{API_NAMES['pixella']['name']}:{pixella_username}\n"
-                    text += f"{API_NAMES['toggl']['token']}:{toggl_token}\n"
-                    text += f"{API_NAMES['toggl']['workspace']}:{toggl_workspace_id}"
+                    text = create_env_text(
+                        toggl_token, toggl_workspace_id, pixella_token, pixella_username)
                     file_success = create_env_file(text)
+
                     if file_success:
                         print('File created!')
             else:
-                print("Sorry, but without correct information about your Toggl account, this app is unable to fulfill its function.")
-                user_input = input('Would you like to:\n1. Restart the app\n2.Close the app\nEnter 1 or 2:   ')
-                if user_input == '1':
-                    start_app()
-                elif user_input == '2':
-                    pass
-                
+                prompt = "Sorry, but without correct information about your Toggl account, this app is unable to fulfill its function."
+                restart_prompt(prompt)
+                # user declines to provide information
+        else:
+            prompt = 'Sorry, but without information about your accounts, this app is unable to fulfill its function.'
+            restart_prompt(prompt)
 
+    # FILE EXISTS
+    else:
+        pass
 
-# # check if the file exists
-# if FILE_EXISTS:
-#     with open(ENV_FILE) as file:
-#         lines = file.readlines()
-#         for line in lines:
-#             print(line)
-
-# # if there's no file then get info
-# else:
-#     user_input = input(
-#         "There's no information about your Toggl or Pixella accounts.\nWould you like to add it? (y/n)   "
-#     ).lower()
-#     if user_input in YES:
-#         toggl_token, toggl_workspace_id = get_toogl_info()
-#         text = ""
-
-#         print(text)
-#     else:
-#         print("Sorry, please come back later.")
-
+clear()
+print("Welcome to Toggl to Pixella app!\n")
 start_app()
-# get_toogl_info()
