7b900618909a8f64dc417772262cf4e2fb88374b
ksnldn <92630098+ksnldn@users.noreply.github.com>
2024-02-07 11:13:54 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "requests",
-        "datetime",
-        "json",
-        "dateutil",
-        "decouple",
-        "pandas",
-        "base64",
+        "requests", "datetime", "json", "dateutil", "decouple", "pandas",
+        "base64", "numpy", "logging", "re", "os", "sys", "dotenv"
     ],
 )
